9ea98b6e5dc89806b3cdf2aab060ced840addcd7
mindex <dev@mindex.com>
2024-07-29 13:42:20 -0400

    Adding task 1 tests
    Added tests for task 1

--- src/test/java/com/mindex/challenge/service/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/EmployeeServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +16,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -24,6 +27,7 @@ public class EmployeeServiceImplTest {
 
     private String employeeUrl;
     private String employeeIdUrl;
+    private String numberOfReportsUrl;
 
     @Autowired
     private EmployeeService employeeService;
@@ -38,6 +42,7 @@ public class EmployeeServiceImplTest {
     public void setup() {
         employeeUrl = "http://localhost:" + port + "/employee";
         employeeIdUrl = "http://localhost:" + port + "/employee/{id}";
+        numberOfReportsUrl = "http://localhost:" + port + "/employee/numberOfReports/{id}";
     }
 
     @Test
@@ -75,6 +80,79 @@ public class EmployeeServiceImplTest {
                         readEmployee.getEmployeeId()).getBody();
 
         assertEmployeeEquivalence(readEmployee, updatedEmployee);
+    }
+
+    @Test
+    public void testNumberOfReportsNoSubReports() {
+        Employee testEmployee1 = new Employee();
+        Employee testEmployee2 = new Employee();
+        Employee testEmployee3 = new Employee();
+        Employee testEmployee4 = new Employee();
+        Employee testEmployee5 = new Employee();
+
+        ArrayList<Employee> directReports1 = new ArrayList<Employee>();
+        ArrayList<Employee> directReports2 = new ArrayList<Employee>();
+        ArrayList<Employee> directReports3 = new ArrayList<Employee>();
+
+        testEmployee2.setEmployeeId("b");
+        testEmployee3.setEmployeeId("c");
+        testEmployee4.setEmployeeId("d");
+        testEmployee5.setEmployeeId("e");
+
+        //setting employees with no direct reports for the first test
+        directReports1.add(testEmployee2);
+        directReports1.add(testEmployee3);
+        directReports1.add(testEmployee4);
+        directReports1.add(testEmployee5);
+
+        testEmployee1.setDirectReports(directReports1);
+
+        Employee storedTestEmployee = restTemplate.postForEntity(employeeUrl, testEmployee1, Employee.class).getBody();
+        assertNotNull(storedTestEmployee.getEmployeeId());
+
+        ReportingStructure numberOfReports = restTemplate.getForEntity(numberOfReportsUrl, ReportingStructure.class, storedTestEmployee.getEmployeeId()).getBody();
+
+        //testEmployee1 has 4 direct reports, so the numberOfReports should be 4
+        assertEquals(numberOfReports.getNumberOfReports(), 4);
+    }
+
+    @Test
+    public void testNumberOfReportsWithSubReports() {
+        Employee testEmployee1 = new Employee();
+        Employee testEmployee2 = new Employee();
+        Employee testEmployee3 = new Employee();
+        Employee testEmployee4 = new Employee();
+        Employee testEmployee5 = new Employee();
+
+        ArrayList<Employee> directReports1 = new ArrayList<Employee>();
+        ArrayList<Employee> directReports2 = new ArrayList<Employee>();
+
+        testEmployee2.setEmployeeId("b");
+        testEmployee3.setEmployeeId("c");
+        testEmployee4.setEmployeeId("d");
+        testEmployee5.setEmployeeId("e");
+
+        //making an employee with a duplicate direct report
+        directReports1.add(testEmployee4);
+        directReports1.add(testEmployee5);
+
+        testEmployee2.setDirectReports(directReports1);
+
+        directReports2.add(testEmployee2);
+        directReports2.add(testEmployee3);
+        directReports2.add(testEmployee4);
+        directReports2.add(testEmployee5);
+
+        //testEmployee1 should have ids 'b', 'c', 'd', and 'e' as direct reports, but id 'b' should also have 'd' and 'e' as direct reports, so they shouldn't be counted twice
+        testEmployee1.setDirectReports(directReports2);
+
+        Employee storedTestEmployee = restTemplate.postForEntity(employeeUrl, testEmployee1, Employee.class).getBody();
+        assertNotNull(storedTestEmployee.getEmployeeId());
+
+        ReportingStructure numberOfReports = restTemplate.getForEntity(numberOfReportsUrl, ReportingStructure.class, storedTestEmployee.getEmployeeId()).getBody();
+
+        //testEmployee1 has 6 direct reports, but 2 of the direct reports repeat in the nested arrays, so the actual result should be 4
+        assertEquals(numberOfReports.getNumberOfReports(), 4);
     }
 
     private static void assertEmployeeEquivalence(Employee expected, Employee actual) {
